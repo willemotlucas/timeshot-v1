ad71f3a56d50300c538c3cd04ed15ca3c2d7a312
Paul Jeannot <gum@MacBook-Pro-de-Paul-2.local>
2016-03-17 10:04:45 +0100

    Ajout du Podfile + Camera Manager

--- TimeshotApp/TimeshotApp/ViewController.swift
+++ TimeshotApp/TimeshotApp/ViewController.swift
@@ -10,18 +10,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // user d'un enorme staff ici.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
