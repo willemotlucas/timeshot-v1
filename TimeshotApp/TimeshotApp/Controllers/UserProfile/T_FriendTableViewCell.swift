05527181cab0e21c645d7050732bcf06b11c9317
Lucas Willemot <willemotlucas@gmail.com>
2016-03-22 15:42:00 +0100

    Fix issues on missing files and folders

---
+++ TimeshotApp/TimeshotApp/Controllers/UserProfile/T_FriendTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  T_FriendTableViewCell.swift
+//  TimeshotApp
+//
+//  Created by Lucas Willemot on 22/03/2016.
+//  Copyright © 2016 Timeshot. All rights reserved.
+//
+
+import UIKit
+
+class T_FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var friendProfileImage: UIImageView!
+    @IBOutlet weak var friendNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
