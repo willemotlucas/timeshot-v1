bb443ff4efa1d94af278e9660fba82a072e030b1
Paul Jeannot <paul.jeannot@etu.utc.fr>
2016-04-06 13:32:31 +0200

    Start to implement parse : OK ; Podfile : OK ; Login : OK : PFUser : not finished

--- TimeshotApp/TimeshotApp/Controllers/Camera/T_AddFriendToAlbumTableViewCell.swift
+++ TimeshotApp/TimeshotApp/Controllers/Camera/T_AddFriendToAlbumTableViewCell.swift
@@ -23,6 +23,5 @@ class T_AddFriendToAlbumTableViewCell: UITableViewCell {
     
     deinit
     {
-//        print("Cells")
     }
 }
---
+++ TimeshotApp/TimeshotApp/Models/T_User.swift
@@ -0,0 +1,53 @@
+//
+//  T_Friend.swift
+//  TimeshotApp
+//
+//  Created by Paul Jeannot on 05/04/2016.
+//  Copyright © 2016 Timeshot. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class T_User : PFUser {
+    
+    @NSManaged var emailVerified:Bool
+    @NSManaged var birthDate:NSDate
+    @NSManaged var firstName:String
+    @NSManaged var lastName:String
+    @NSManaged var isDeleted:Bool
+    
+    
+    
+    //MARK: PFSubclassing Protocol
+    override init () {
+        super.init()
+    }
+    
+    init(username: String, password:String, birthDate:NSDate, email:String, firstName:String, lastName:String)
+    {
+        super.init()
+        
+        self.username = username
+        self.password = password
+        self.birthDate = birthDate
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        self.isDeleted = false
+    }
+    
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            // inform Parse about this subclass
+            self.registerSubclass()
+        }
+    }
+    
+    func createUser()
+    {
+        saveInBackgroundWithBlock(nil)
+    }
+    
+}
